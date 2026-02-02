781978713d28acb9a65cb406912f9e10cdb1dc0e
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-02 20:00:03 +0530

    Exported the Redux store and the types of the state and the store's dispatch function

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartSlice } from "./cartSlice";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
   },
 });
+
+// Getting the type of the app state stored by the Redux store --> ReturnType is a built-in TS utility type to access the return type of a function type. The getState method returns the state held by the store. So, to assign the type of the store's state to RootState, we access the type of the return value of the getState method:
+export type RootState = ReturnType<typeof store.getState>;
+
+// Creating a base type from the type Redux's dispatch function:
+export type AppDispatch = typeof store.dispatch;

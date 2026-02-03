6e2ef961914260e9a836dc28786f2f73bf0d5f69
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-03 15:11:01 +0530

    Added/improved explanatory comments

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,8 +7,10 @@ export const store = configureStore({
   },
 });
 
-// Getting the type of the app state stored by the Redux store --> ReturnType is a built-in TS utility type to access the return type of a function type. The getState method returns the state held by the store. So, to assign the type of the store's state to RootState, we access the type of the return value of the getState method:
+// Getting the type of the app's state that's stored by Redux --> ReturnType is a built-in TS utility type to access the return type of a function type. The getState method returns the state held by the Redux store. So, to assign the type of the store's state to RootState, we access the type of the return value of the getState method:
 export type RootState = ReturnType<typeof store.getState>;
+// The RootState type is then used as the type argument of TypedUseSelectorHook<T>, which is a type provided by react-redux to create a custom state selector that adds some store-state-specific type awareness to the regular selector provided by the useSelector Redux hook (see useAppSelector in hooks.ts).
 
-// Creating a base type from the type Redux's dispatch function:
+// Creating a type for the return value of a custom, app-state-specific dispatch hook with type awareness, from the type of Redux store's dispatch function:
 export type AppDispatch = typeof store.dispatch;
+// The AppDispatch type is used as the return type of the function type of the custom dispatch hook (see useAppDispatch in hooks.ts)
